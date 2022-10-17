8da9dde0fd8f480d50c33045968b07495c75208e
Sorokin Mikhail <ip.sorokin1738@mail.ru>
2022-10-17 21:49:10 +0300

    Added code in Task4

--- Task4.py
+++ Task4.py
@@ -1,18 +1,22 @@
 # 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
 # Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 
-N=int(input('Введите сколько должно быть элементов : '))
-res = 0
-for i in range(-N, N):
-    
-    f = open('file.txt','r')
-    for l in f:
-        if l == '':
-           break
-        else:
-            i=l
-            res*=i[l]    
-
-
+N=int(input('Введите N : '))
+list=[]
+for i in range(-N-1,N):
+    i+=1
+    list.append(i)
+print(list)
+f = open('file.txt','r')
+res = 1
+for l in f:
+    l=int(l)
+    if l > len(list):
+        print(f' Нет {l} позиции в этом списке')
+    else:
+        res*=list[l]
+    print(list[l] , end=',')
+        
 f.close()
-print(res)
+
+print(f'Произведение элементов на указанных позициях -> {res}')
--- Task5.py
+++ Task5.py
@@ -1,8 +1,9 @@
 # 5. Реализуйте алгоритм перемешивания списка.
 spisok1=[1,2,3,4,5,6]
+print(f'Исходный список ->{ spisok1}')
 spisok2=[]
 for i in spisok1:
     i-=1
     spisok2.insert(i-len(spisok1),spisok1[i])
 
-print(spisok2)
+print(f'Перемешанный список -> {spisok2}')
